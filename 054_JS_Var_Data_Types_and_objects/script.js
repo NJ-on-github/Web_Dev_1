9a0af3ac3942c8afb751dc331d960be5a3beab51
Nishit Jain <nishit12345.jain@gmail.com>
2024-05-20 01:37:20 +0530

    Netflix Clone directory rename + Vid 54

---
+++ 054_JS_Var_Data_Types_and_objects/script.js
@@ -0,0 +1,28 @@
+var a = 5;
+var b = 11;
+
+console.log(a + b + 4); // adds the variables
+
+var c ="string"; //a string in double quotes
+var d = 'another string'; //a string in single quotes
+var e = `yet another string`; //a string in backticks
+
+console.log(typeof c, typeof a, typeof b); //prints the type of the variable,
+
+const c1 = 5; //a constant
+//same variable name rules are applied as c++.
+
+//let is block level
+//var is global level
+
+//DATA Types
+
+// Primitive Data Types
+// Number
+// String
+// Boolean
+// Null
+// Undefined
+// BigInt
+// Symbol
+
---
+++ 054_Javascript_Introduction/script.js
@@ -0,0 +1,18 @@
+alert("Alert");
+
+console.log("Testing");
+
+var a = prompt("Enter value of variable A");
+console.log("Val of A as entered by user" + a);
+
+var isTrue = confirm("Say yes");
+
+if(isTrue){
+    console.log("Good!!")
+}
+else{
+    console.log("Say YES")
+}
+
+document.title = "Newww Title";
+document.body.style.backgroundColor = "red"; //adds inline CSS in html
