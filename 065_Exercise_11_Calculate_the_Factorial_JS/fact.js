1b264a9e85ca98d449425165557f44ef8b7c6e76
Nishit Jain <nishit12345.jain@gmail.com>
2024-06-03 23:19:34 +0530

    Vid 64 Exercise 11 Solution

--- 065_Exercise_11_Calculate_the_Factorial_JS/fact.js
+++ 065_Exercise_11_Calculate_the_Factorial_JS/fact.js
@@ -1,1 +1,13 @@
 let arr = [];
+
+let n = prompt("Enter your number to find factorial of -> ");
+
+for (let i = 1; i <= n; i++) {
+    arr.push(i);
+}
+
+const mul = (a,b) => {
+    return a*b;
+}
+
+console.log(arr.reduce(mul));
