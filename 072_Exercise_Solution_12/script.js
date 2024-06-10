579c759f2b53231da5a5a4b70f2da9e7af090483
Nishit Jain <nishit12345.jain@gmail.com>
2024-06-11 00:52:00 +0530

    Vid 72

---
+++ 072_Exercise_Solution_12/script.js
@@ -0,0 +1,30 @@
+// console.log(document.getElementsByClassName("box"));
+// let temp = document.getElementsByClassName("box");
+
+
+// // I did just a little differently 
+// //Math.ceil
+
+
+
+// Analyzing somebody's else's answer
+// let letter = "123456789ABCDEF"
+// function nam() {
+//     let lettef = "0123456789"
+//     let letter = "123456789ABCDEF"
+//     let color = "#"
+//     for (let i = 0; i < 6; i++) {
+//         let r = Math.floor(Math.random()*16-1);
+//         color += letter [r]
+//         console.log(r);
+//     }
+//     console.log(color);
+//     return color
+//   }
+
+// //   console.log('nam()');
+//   console.log(nam());
+// //   console.log(Math.floor(Math.random()*16));
+
+// console.log(letter[14]);
+
