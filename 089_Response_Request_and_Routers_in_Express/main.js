5241e74ba8f02e347dbcdcee8cf80ac8c1ff832c
Nishit Jain <nishit12345.jain@gmail.com>
2025-04-18 23:54:10 +0530

    Vid 90

--- 089_Response_Request_and_Routers_in_Express/main.js
+++ 089_Response_Request_and_Routers_in_Express/main.js
@@ -5,7 +5,33 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.post('/', (req, res) => {
+    res.send('Post request received!')
+})
+app.put('/', (req, res) => {
+    res.send('Put request received!')
+})
+app.delete('/', (req, res) => {
+    res.send('Delete request received!')
+})
+
+
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', { root: __dirname })
+})
+
+app.get('/', (req, res) => {
+  res.json({x:1, y:2});
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+//browsers by default use get request
+
+
+//sendingFiles
+app.get('/files' , (req, res)=>{
+    res.download('./templates/index.html', { root: __dirname })
 })
